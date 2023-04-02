8f6744aa3e6771a2ed2b8c2cfc9514f3610999aa
alexandremsouza1 <pedestaljunior@gmail.com>
2023-04-02 14:49:59 +0000

    Deploying to gh-pages from  @ 0393dd898e243866c361fa1b97d7473d8b15624e 🚀

---
+++ precache-manifest.d1717ca6de64f71c390217aa550acbd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b887341ca670571fe1",
+    "url": "/curriculum-me/static/css/main.f29a5162.chunk.css"
+  },
+  {
+    "revision": "39b887341ca670571fe1",
+    "url": "/curriculum-me/static/js/main.39b88734.chunk.js"
+  },
+  {
+    "revision": "f1f5424402acf7d7987f",
+    "url": "/curriculum-me/static/js/runtime~main.f1f54244.js"
+  },
+  {
+    "revision": "5efd7df7a6c2f77d4d87",
+    "url": "/curriculum-me/static/js/2.5efd7df7.chunk.js"
+  },
+  {
+    "revision": "d73c99cfe0a33302d1be7f8ed30adb9a",
+    "url": "/curriculum-me/index.html"
+  }
+];
