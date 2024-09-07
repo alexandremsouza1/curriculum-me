61a9d1916984411e5f840239c1a231508f8838f8
Alexandre Magno <pedestaljunior@gmail.com>
2024-09-07 10:35:05 -0300

    ajusts 72 score

--- src/pages/NewResume.jsx
+++ src/pages/NewResume.jsx
@@ -1,87 +1,112 @@
-import { Page, StyleSheet, Text, View } from '@react-pdf/renderer';
+import { Page, StyleSheet, Text, View,Image } from '@react-pdf/renderer';
 import React from 'react';
 
 // Create styles
 const styles = StyleSheet.create({
-    page: {
-        with: 100,
-        flexDirection: 'column',
-        fontSize: 10,
-        fontFamily: 'Lato',
-        backgroundColor: '#FFFFFF',
-        color: '#023642',
-        padding: 50,
-        margin: 10,
-        lineHeight: 1.5,
-        letterSpacing: 1,
-    },
-    sectionTitle: {
-        marginTop: 20,
-        color: '#305fec',
-        fontFamily: 'SourceSansPro-Bold',
-        textTransform: 'uppercase',
-        borderBottomWidth: 1,
-        borderBottomColor: '#000',
-        marginBottom: 1,
-    }, 
-    title: {
-        fontFamily: 'SourceSansPro-Bold',
-    },
-    sectionContent: {
-        color: '#023642',
-    },
-    job: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        width: 100,
-    },
-    jobCompany: {
-        fontFamily: 'SourceSansPro-Bold',
-    },
-    jobSectionContent: {
-        color: '#023642',
-        marginBottom: 5,
-    },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    content: {
-        flexDirection: 'column',
-        flex: 1,
-    },
-    name: {
-        fontSize: 18,
-        textAlign: 'left',
-        alignItems: 'flex-start',
-        justifyContent: 'flex-end',
-        color: '#305fec',
-        lineHeight: 0,
-        letterSpacing: 1,
-    },
-    contactdetails: {
-        justifyContent: 'flex-end',
-        textAlign: 'left',
-    },
-    logo: {
-        width: 50,
-        height: 50,
-        backgroundColor: 'blue',
-        fontFamily: 'SourceSansPro-Bold',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end',
-    },
-    initials: {
-        color: 'white',
-        fontSize: 20,
-    }
+  page: {
+    with: 100,
+    flexDirection: "column",
+    fontSize: 10,
+    fontFamily: "Lato",
+    backgroundColor: "#FFFFFF",
+    color: "#023642",
+    padding: 50,
+    margin: 10,
+    lineHeight: 1.5,
+  },
+  sectionTitle: {
+    marginTop: 20,
+    color: "#305fec",
+    fontFamily: "SourceSansPro-Bold",
+    textTransform: "uppercase",
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+    marginBottom: 1,
+  },
+  title: {
+    fontFamily: "SourceSansPro-Bold",
+  },
+  sectionContent: {
+    color: "#023642",
+  },
+  job: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: 100,
+  },
+  jobCompany: {
+    fontFamily: "SourceSansPro-Bold",
+  },
+  jobSectionContent: {
+    color: "#023642",
+    marginBottom: 5,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  content: {
+    flexDirection: "column",
+    flex: 1,
+  },
+  name: {
+    fontSize: 18,
+    textAlign: "left",
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    color: "#305fec",
+    lineHeight: 0,
+    flexGrow: 1,
+    textAlign: "left",
+    marginLeft: 10,
+  },
+  contactdetails: {
+    flexDirection: "column",
+    textAlign: "left",
+  },
+  contactItem: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  icon: {
+    width: 8,
+    height: 8,
+    marginRight: 2,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    backgroundColor: "blue",
+    fontFamily: "SourceSansPro-Bold",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  initials: {
+    color: "white",
+    fontSize: 20,
+  },
+  skillsContainer: {
+    marginTop: 10,
+  },
+  skillsTitle: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  skillsList: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  skillItem: {
+    fontSize: 10,
+  },
 });
 
 const contactInfo = {
     firstName: "Alexandre",
     lastName: "Magno",
     email: "alexandremagnodev0@gmail.com",
-    phone: "+55(37)9 99492674",
+    phone: "(37)9 9949-2674",
     location: "Minas Gerais, Brazil",
 }
 
@@ -105,27 +130,43 @@ const sections = [
         jobs: [
             {
                 "company": "Mobiup Beyond Digital",
-                "period": "Jan 2022 - Jul 2024, Sao Paulo",
+                "period": "May 2022 - Jul 2024, Sao Paulo",
                 "role": "Full-Stack Web-Developer",
-                "description": "Web Developer focused on the maintenance and enhancement of e-commerce projects, blogs, and payment gateway integrations. Responsible for leading improvements, implementing new features, and performing fixes, ensuring system stability and efficiency. I actively participated in the company's transition from a monolithic e-commerce architecture to microservices, including the implementation of a container orchestration infrastructure using Envoy."
+                "description": 
+                `- Worked in the support and projects team, contributing to various e-commerce initiatives.
+                - Participated in a project for a platform offering exclusive benefits and loyalty points for purchases.
+                - Developed solutions for a ticketing system, integrating various services and APIs.
+                - Actively contributed to Coca-Cola’s e-commerce platform, focused on the company’s products.
+                - Took part in a large-scale project involving the migration of a legacy system to a microservices architecture, covering both backend and application migration.
+                - Implemented performance, security, and scalability improvements during the migration process, using modern software development practices.`
             },
             {
                 "company": "Trackerup Tecnologia LTDA",
-                "period": "Jan 2018 - Jan 2022, Sao Paulo",
+                "period": "Apr 2018 - May 2022, Sao Paulo",
                 "role": "Software Developer",
-                "description": "I was responsible for maintaining and enhancing a task tracking and routing system for field employees. My activities included fixing bugs, implementing new features, optimizing system performance, and ensuring the accuracy of location and routing data, which improved the efficiency of external operations."
+                "description": 
+                `- Worked on the maintenance of the TrackerUp application tracking project, which allows advanced coordination of external teams in a fast and streamlined way. Key functionalities include: real-time task management, route suggestions, history of traveled routes, intelligent reporting
+                 - Participated in the implementation of a large project for Whirlpool, managing tasks for employees responsible for maintaining white goods appliances.
+                 - Contributed to the implementation of a project for Sanofi, calculating customer routes based on a schedule of visits to optimize costs for the company`
             },
             {
                 "company": "W.E Logus Informática LTDA",
-                "period": "Jan 2017 - Jan 2017, Formiga",
+                "period": "Dez 2016 - Nov 2017, Formiga",
                 "role": "T.I Assistant",
-                "description": "I contributed to the company's CRM system by ensuring quality control through comprehensive testing, managing backup processes to safeguard data integrity, and delivering exceptional customer support. My role was essential in maintaining system reliability and providing clients with seamless experiences."
+                "description": 
+                `- Initially worked in customer service and support before transitioning to the quality department.
+                 - Developed metrics to evaluate customer service performance and collected data to assess the acceptance of new features and the expected outcomes.
+                 - Tested features deployed to the Eficaz ERP CRM to identify any side effects or issues.
+                 - Responsible for database migration and ensuring data integrity during transitions.`
             },
             {
                 "company": "SOL Logística e Locações LTDA ME",
-                "period": "Jan 2016 - Jan 2016, Belo Horizonte",
+                "period": "Apr 2016 - Aug 2016, Belo Horizonte",
                 "role": "Administrative Assistant",
-                "description": "I was responsible for issuing Electronic Transport Knowledge (CTe) documents, managing load control, and preparing balance sheets and reports. My role was crucial in ensuring accurate documentation and efficient logistics operations."
+                "description": 
+                `- Managed the issuance of tax documents related to cargo operations.
+                 - Controlled internal operations for the Vilma Flour project, overseeing inventory management for incoming and outgoing products in the warehouse.
+                 - Participated in the installation of the internal network and configuration of servers to support operational needs.`
             }
         ]
     },
@@ -138,6 +179,7 @@ const sections = [
     {
         order: 5,
         name: "Skills",
+        list: ['Vue', 'React', 'Laravel', 'React-Native', 'Expo', 'PHP', 'Go', 'NodeJS', 'Javascript', 'Typescript','Docker', 'Kubernetes']
 
     }
 ]
@@ -153,43 +195,78 @@ function addSpacesToName(name) {
     return characters.join(' ');
 }
 
+const renderExperience = (section) => (
+    <View>
+        {section.jobs.map((job, jobIndex) => (
+            <View key={jobIndex}>
+                <Text style={styles.jobCompany}> • {job.company} | {job.role} | {job.period}</Text>
+                <Text style={styles.jobSectionContent}>{job.description}</Text>
+            </View>
+        ))}
+    </View>
+);
+
+const renderSkills = (section) => (
+    <View style={styles.skillsContainer}>
+        <Text style={styles.skillsList}>
+            {section.list.map((skill, index) => (
+                <Text key={index} style={styles.skillItem}>
+                    { ' • ' + skill}
+                </Text>
+            ))}
+        </Text>
+    </View>
+);
+
+const sectionRenderers = {
+    "Work Experience": renderExperience,
+    "Skills": renderSkills,
+ 
+};
+
+const renderSection = (section) => {
+    const renderFunction = sectionRenderers[section.name];
+    if (renderFunction) {
+        return renderFunction(section);
+    }
+    return <Text style={styles.sectionContent}>{section.content}</Text>;
+};
+
 const sortedSections = [...sections].sort((a, b) => a.order - b.order);
 
 const ResumePage = () => (
-    <Page size="A4" style={styles.page}>
-        <View style={styles.header}>
-            <View style={styles.logo}>
-                <Text style={styles.initials}>{geNameInitials()}</Text>
-            </View>
-            <View style={styles.name}>
-                <Text>{addSpacesToName(contactInfo.firstName)}</Text>
-                <Text>{addSpacesToName(contactInfo.lastName)}</Text>
-            </View>
-            <View style={styles.contactdetails}>
-                <Text>{contactInfo.phone}</Text>
-                <Text>{contactInfo.email}</Text>
-                <Text>{contactInfo.location}</Text>
-            </View>
+  <Page size="A4" style={styles.page}>
+    <View style={styles.header}>
+      <View style={styles.logo}>
+        <Text style={styles.initials}>{geNameInitials()}</Text>
+      </View>
+      <View style={styles.name}>
+        <Text>{addSpacesToName(contactInfo.firstName)}</Text>
+        <Text>{addSpacesToName(contactInfo.lastName)}</Text>
+      </View>
+      <View style={styles.contactdetails}>
+        <View style={styles.contactItem}>
+          <Image style={styles.icon} src="assets/icon-phone.png" />
+          <Text>{ contactInfo.phone }</Text>
         </View>
-        {sortedSections.map((section,index) => (
-            <View key={index} style={styles.section}>
-                <Text style={styles.sectionTitle}>{section.name}</Text>
-                {section.title && <Text style={styles.title}>{section.title}</Text>}
-                {section.name === "Work Experience" ? (
-                    <View>
-                        {section.jobs.map((job, jobIndex) => (
-                            <View key={jobIndex}>
-                                <Text style={styles.jobCompany}> • {job.company} | {job.role} | {job.period}</Text>
-                                <Text style={styles.jobSectionContent}>{job.description}</Text>
-                            </View>
-                        ))}
-                    </View>
-                ) : (
-                    <Text style={styles.sectionContent}>{section.content}</Text>
-                )}
-            </View>
-        ))}
-    </Page>
+        <View style={styles.contactItem}>
+          <Image style={styles.icon} src="assets/icon-mail.png" />
+          <Text>{ contactInfo.email }</Text>
+        </View>
+        <View style={styles.contactItem}>
+          <Image style={styles.icon} src="assets/icon-location-pin.png" />
+          <Text>{ contactInfo.location }</Text>
+        </View>
+      </View>
+    </View>
+    {sortedSections.map((section, index) => (
+      <View key={index} style={styles.section}>
+        <Text style={styles.sectionTitle}>{section.name}</Text>
+        {section.title && <Text style={styles.title}>{section.title}</Text>}
+        {renderSection(section)}
+      </View>
+    ))}
+  </Page>
 );
 
 export default ResumePage;
